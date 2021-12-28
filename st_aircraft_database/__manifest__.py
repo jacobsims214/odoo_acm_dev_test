c8870c34710b66fcf7617aa4cc121798140e0bb7
jacobsims214 <Jacob.sims@simstech.cloud>
2021-12-28 18:04:56 -0500

    setting up dev files

---
+++ st_aircraft_database/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Aircraft Database',
+    'version' : '15.0.1.0',
+    'summary': 'SimsTech AirCraft DataBase Manager',
+    'sequence': 10,
+    'description': """
+Aircraft Database Manager
+====================
+This is a dev version of application
+    """,
+    'category': '',
+    'website': 'https://simstech.cloud',
+    'images' : [],
+    'depends' : [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
